81278063381d46cace8ae44f486d92fd5adb062e
Josh Jacobson <joshhjacobson@gmail.com>
2021-03-10 14:49:49 -0500

    Improving output of clues and guesses.

--- mastermind.rb
+++ mastermind.rb
@@ -3,10 +3,17 @@
 module Mastermind
   CODE_LENGTH = 4
   TURNS_PER_ROUND = 12
+  BOTH_CORRECT_SYM = 'x'.freeze
+  COLOR_CORRECT_SYM = 'o'.freeze
+
+
+
 end
 
 # extending string to include colors
 class String
+  include Mastermind
+
   RESET_CODE = "\u001b[0m".freeze
   BOLD = "\u001b[1m".freeze
   GREEN_BG = "\u001b[42m".freeze
@@ -15,6 +22,8 @@ class String
   BLUE_BG = "\u001b[46m".freeze
   ORANGE_BG = "\u001b[43m".freeze
   WHITE_BG = "\u001b[47m".freeze
+  BIG_DOT = "\u2b24".freeze
+  ORANGE_TEXT = "\u001b[31m".freeze
 
   def format
     case self
@@ -24,36 +33,44 @@ class String
     when '3' then format_three
     when '4' then format_four
     when '5' then format_five
+    when BOTH_CORRECT_SYM then format_both_correct
+    when COLOR_CORRECT_SYM then format_color_correct
     else self
     end
   end
 
-  def format_one
-    "#{BOLD}#{GREEN_BG} #{self} #{RESET_CODE}"
-  end
+  private
 
-  def test
-    format_one
+  def format_one
+    " #{BOLD}#{GREEN_BG} #{self} #{RESET_CODE} "
   end
 
   def format_two
-    "#{BOLD}#{RED_BG} #{self} #{RESET_CODE}"
+    " #{BOLD}#{RED_BG} #{self} #{RESET_CODE} "
   end
 
   def format_three
-    "#{BOLD}#{PINK_BG} #{self} #{RESET_CODE}"
+    " #{BOLD}#{PINK_BG} #{self} #{RESET_CODE} "
   end
 
   def format_four
-    "#{BOLD}#{BLUE_BG} #{self} #{RESET_CODE}"
+    " #{BOLD}#{BLUE_BG} #{self} #{RESET_CODE} "
   end
 
   def format_five
-    "#{BOLD}#{ORANGE_BG} #{self} #{RESET_CODE}"
+    " #{BOLD}#{ORANGE_BG} #{self} #{RESET_CODE} "
   end
 
   def format_zero
-    "#{BOLD}#{WHITE_BG} #{self} #{RESET_CODE}"
+    " #{BOLD}#{WHITE_BG} #{self} #{RESET_CODE} "
+  end
+
+  def format_both_correct
+    "#{BOLD}#{ORANGE_TEXT} #{BIG_DOT} #{RESET_CODE}"
+  end
+
+  def format_color_correct
+    "#{BOLD} #{BIG_DOT} #{RESET_CODE}"
   end
 end
 
@@ -91,7 +108,11 @@ class Code
   end
 
   def to_s
-    code.join(' ' * 3)
+    new_str = ''
+    code.each do |element|
+      new_str += element.to_s.format
+    end
+    new_str
   end
 
   private
@@ -121,7 +142,12 @@ class Clues
   end
 
   def to_s
-    ('x' * both_correct).concat('o' * color_correct).gsub(/[xo]/) { |c| " #{c} " }
+    t_arr = ([BOTH_CORRECT_SYM] * both_correct).concat([COLOR_CORRECT_SYM] * color_correct)
+    new_str = ''
+    t_arr.each do |element|
+      new_str += element.to_s.format
+    end
+    new_str
   end
 
   private
@@ -161,23 +187,30 @@ def ask_guess(round)
 end
 
 def valid_input?(user_guess)
-  user_guess.match?('[0-5]{4}')
+  user_guess.match?('\A[0-5]{4}\z')
 end
 
 def process_round_end(round, clue)
   if clue.both_correct == 4
-    puts 'You win!'
+    puts "You win! The code was #{round.answer}"
   else
-    puts "You lose :(. The code was #{round}"
+    puts "You lose :(. The code was #{round.answer}"
   end
 end
 
 def play_round
+  print_instructions
   round = Round.new
   until round.complete
     guess = ask_guess(round)
     clue = round.process_guess(guess.code)
-    puts clue
+    puts "#{guess}  Clues: #{clue}"
   end
   process_round_end(round, clue)
+end
+
+def print_instructions
+  puts 'This is the game of Mastermind.'
+  puts 'You have 12 rounds to guess the correct code. Each time you guess, you will receive feedback on the number of colors and positions you have guessed correctly as well as the number of colors you have guessed correctly.'
+  puts "For each correct color and position, you will see a #{'x'.format}. And for each color you have correct, you will see a #{'o'.format}. \n"
 end
